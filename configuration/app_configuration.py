e3304c9a2e497b89f639b01007f5d3b4885fa97a
Filip Ivković <fivkovic@uns.ac.rs>
2021-09-12 01:17:03 +0200

    Add initial database configuration

--- configuration/app_configuration.py
+++ configuration/app_configuration.py
@@ -1,8 +1,17 @@
+from os import environ
+
 from flask.app import Flask
 from flask_restful import Api
 
+db_uri_configuration = {
+    'test': 'TEST_DATABASE_URI',
+    'development': 'DEVELOPMENT_DATABASE_URI'
+}
+
 
 def get_configured_app(configuration_name: str):
+    environ['SQLALCHEMY_DATABASE_URI'] = environ.get(db_uri_configuration[configuration_name])
+
     app = Flask(__name__)
 
     # TODO (fivkovic): Configure CSRF
@@ -10,7 +19,10 @@ def get_configured_app(configuration_name: str):
 
     restful_api = Api(app)
 
-    # TODO (fivkovic): Initialize DB
+    from configuration.database_configuration import initialize_database
+    initialize_database()
+
     # TODO (fivkovic): Add resources mapping to API
 
     return app
+

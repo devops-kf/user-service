5681777466c11b13fbd95fd9d7b0d5e4d33b57b6
Filip Ivković <fivkovic@uns.ac.rs>
2021-09-12 13:12:07 +0200

    Add registration request validation schemas (WIP)

---
+++ schemas/registration.py
@@ -0,0 +1,24 @@
+from marshmallow import validate, fields
+
+from schemas.base import RequestBodyParamsSchema
+
+
+class RegistrationRequestBodyParamsSchema(RequestBodyParamsSchema):
+    username = fields.String(required=True, validate=validate.Length(min=6, max=30))
+    password = fields.String(required=True, validate=validate.Length(min=6, max=30))
+    email = fields.Email(required=True)
+    account_type = fields.Integer(required=True, validate=validate.OneOf([0, 1]),
+                                  error_messages={'out_of_range': "Unsupported account type."})
+    profile_image_url = fields.URL(required=True, schemes=['http', 'https'],
+                                   error_messages={'unsupported_url': "Invalid profile image URL."})
+    name = fields.String(required=True, validate=validate.Length(min=2, max=60))
+    bio = fields.String(required=True, validate=validate.Length(min=1, max=255))
+    phone_number = fields.String(required=True, validate=validate.Length(min=6, max=30))
+    date_of_birth = fields.Date(required=True)
+    gender = fields.Integer(required=True, validate=validate.OneOf([0, 1]),
+                            error_messages={'unsupported_type': "Unsupported gender type."})
+    country = fields.String(required=True, validate=validate.Length(min=2, max=60))
+    interests = fields.String(required=True, validate=validate.Length(min=1, max=30))
+    shop_url = fields.URL(required=False, schemes=['http', 'https'],
+                          error_messages={'unsupported_url': "Invalid shop URL."})
+

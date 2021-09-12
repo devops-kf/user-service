65947c80ea208f531cf80e84df4b9451c736a3f6
Filip Ivković <fivkovic@uns.ac.rs>
2021-09-12 14:02:56 +0200

    Update API request handler to return objects instead dicts

---
+++ util/api_request_handler.py
@@ -0,0 +1,40 @@
+from flask import request
+from marshmallow import ValidationError
+
+from exception.api_error import ApiError
+from schemas.base import RequestPathParamsSchema, RequestQueryParamsSchema, RequestBodyParamsSchema
+
+
+class ApiRequestHandler:
+
+    @classmethod
+    def __schema_check(cls, schema=None, json_data=None, title=None):
+        try:
+            return schema.load(data=json_data)  # TODO (fivkovic): Add support for partial data - partial=True
+        except ValidationError as err:
+            raise ApiError(status_code=400, title=title, messages=err.messages,
+                           data=err.data, valid_data=err.valid_data)
+
+    @classmethod
+    def handle_request(cls, schemas):
+        if not isinstance(schemas, list):
+            schemas = [schemas]
+
+        path_params_data, query_params_data, body_data = {}, {}, {}
+        for schema in schemas:
+            if isinstance(schema, RequestPathParamsSchema):
+                path_params_data.update(cls.__schema_check(schema=schema, json_data=request.view_args,
+                                                           title='One or more request URL path parameters invalid.'))
+            if isinstance(schema, RequestQueryParamsSchema):
+                query_params_data.update(cls.__schema_check(schema=schema, json_data=request.args,
+                                                            title='One or more request URL query parameters invalid.'))
+            if isinstance(schema, RequestBodyParamsSchema):
+                body_data.update(cls.__schema_check(schema=schema, json_data=request.get_json(),
+                                                    title='One or more request body parameters invalid.'))
+
+        request_data = object()
+        setattr(request_data, 'path_params', path_params_data)
+        setattr(request_data, 'query_params', query_params_data)
+        setattr(request_data, 'body', body_data)
+
+        return request_data
